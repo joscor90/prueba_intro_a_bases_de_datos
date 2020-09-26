1c9c45de7eb32606d17c1d0b76ffa10ba062812f
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-26 11:04:42 -0300

    categorias table creation was added

--- prueba.sql
+++ prueba.sql
@@ -17,3 +17,7 @@ CREATE TABLE fechas(id INT NOT NULL UNIQUE, fecha DATE NOT NULL, PRIMARY KEY (id
 
 CREATE TABLE facturas(numero_factura INT NOT NULL UNIQUE, fecha INT NOT NULL, subtotal INT NOT NULL, cliente INT NOT NULL, PRIMARY KEY (numero_factura), FOREIGN KEY (fecha) REFERENCES fechas(id), FOREIGN KEY (cliente) REFERENCES clientes(id));
 
+-- Creación de tabla "categorias"
+
+CREATE TABLE categorias(id INT NOT NULL UNIQUE, nombre VARCHAR(50) NOT NULL, descripcion VARCHAR(200), PRIMARY KEY (id));
+
