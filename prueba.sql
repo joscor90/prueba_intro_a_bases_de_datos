ec6db6b943e7b2484a9eb74c0cb42ac9fbd78568
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-26 10:43:15 -0300

    Added direcciones and clientes tables creation to prueba.sql

--- prueba.sql
+++ prueba.sql
@@ -1,2 +1,10 @@
--- Creación de base de datos prueba
+-- Creación de base de datos "prueba"
 CREATE DATABASE prueba;
+
+-- Creación de tabla "direcciones"
+
+CREATE TABLE direcciones(id INT NOT NULL UNIQUE, latitud FLOAT NOT NULL, longitud FLOAT NOT NULL, PRIMARY KEY (id));
+
+-- Creación de tabla "clientes"
+
+CREATE TABLE clientes(id INT NOT NULL UNIQUE, nombre VARCHAR(50) NOT NULL, rut INT NOT NULL, direccion INT NOT NULL, PRIMARY KEY (id), FOREIGN KEY (direccion) REFERENCES direcciones(id));
